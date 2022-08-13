288f207b587d9c5aecfefeaca7d19eb93fd6fcce
hfutgf <beretax308@gmail.com>
2022-08-13 16:03:17 +0500

    nmadiram

---
+++ main.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+    salom();
+}, 2000);
+
+
+
+
+function salom() {
+    for (let i = 0; i > -1; i++) {
+      console.log("Jasur browser console liga qara" + " " + i);
+    }
+  }
